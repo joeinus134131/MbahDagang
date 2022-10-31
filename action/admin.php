609b3840d4aec9feda2af51247d1c7073d251ec5
IDN Maker Space <madeagusandi446@gmail.com>
2022-10-31 07:54:54 +0700

    Update admin.php, style.css dan index.php

--- action/admin.php
+++ action/admin.php
@@ -12,6 +12,10 @@ $result = mysqli_query($conn, 'SELECT * FROM barang');
     <link rel="stylesheet" href="style.css" >
     <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+    
+
 </head>
 <header>
     <h1>Dashboard Admin Data</h1>
@@ -26,7 +30,10 @@ $result = mysqli_query($conn, 'SELECT * FROM barang');
     </nav>
 </header>
 <body>
-<a href="add.php">Tambahkan Data</a><br/><br/>
+    <!-- Tombol tambah data -->
+    <!-- <form action="add.php" method="get"> -->
+        <button type="button" class="btn btn-outline-primary">Tambah Data</button>
+    <!-- </form>     -->
     <table>
         <thead>
             <tr>
