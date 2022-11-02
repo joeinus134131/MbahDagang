92f8152d45ab00d82ba789b3b2b303384d189309
IDN Maker Space <madeagusandi446@gmail.com>
2022-11-03 02:56:09 +0700

    Update admin.php, edit.php dan style css

--- action/admin.php
+++ action/admin.php
@@ -13,20 +13,16 @@ $result = mysqli_query($conn, 'SELECT * FROM barang');
     <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    
-    
-
 </head>
 <header>
     <h1>Dashboard Admin Data</h1>
-    <nav class="navbar navbar-expand-lg bg-light">
+    <nav>
         <ul>
-            <li class="nav-item"><a href="http://localhost/MbahDagang/index.php">Home</a></li>
-            <li class="nav-item"><a href="#">Kontak</a></li>
-            <li class="nav-item"><a href="#">Tentang</a></li>
-            <li class="nav-item"><a href="#">Cari</a></li>
-        </ul>
-        
+            <li><a href="http://localhost/MbahDagang/index.php">Home</a></li>
+            <li><a href="#">Kontak</a></li>
+            <li><a href="#">Tentang</a></li>
+            <li><a href="#">Cari</a></li>
+        </ul> 
     </nav>
 </header>
 <body>
