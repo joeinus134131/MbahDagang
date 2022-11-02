24bdf33dd0436cc724430d4a3470333e99839b27
Made Agus Andi Gunawan <madeagusandi446@gmail.com>
2022-11-03 03:39:30 +0700

    Update login php dengan css

--- login.php
+++ login.php
@@ -5,7 +5,9 @@
 ?>
 <html>
     <head>
-
+        <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
     <body>
         <form  name="login" method="get" action="login.php">
