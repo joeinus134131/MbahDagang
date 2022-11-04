c20700be2440d11e8f5779a7d0031cfa2a4e941e
IDN Maker Space <madeagusandi446@gmail.com>
2022-11-04 19:01:51 +0700

    Update semua nav bar

--- action/edit.php
+++ action/edit.php
@@ -38,7 +38,7 @@ while($row = mysqli_fetch_array($result))
 <html>
 <head>	
     <title>Edit Data Barang</title>
-    <link rel = "stylesheet" href="style2.css">
+    <link rel = "stylesheet" href="style.css">
     <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
