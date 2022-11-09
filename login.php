68c38adef62cf39241ba077e345287f5c458fb2e
Made Agus Andi Gunawan <49284230+joeinus134131@users.noreply.github.com>
2022-11-10 05:15:46 +0700

    update login.php

--- login.php
+++ login.php
@@ -1,11 +1,10 @@
 <?php
     include_once("koneksi.php");
+    
     error_reporting(0);
- 
     session_start();
- 
-    if (isset($_SESSION['username'])) {
-        header("Location: action/admin.php");
+    if(isset($_SESSION['username'])){
+        header("Location: admin.php");
     }
     
     if (isset($_POST['submit'])) {
@@ -14,33 +13,36 @@
     
         $sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
         $result = mysqli_query($conn, $sql);
+        echo $result;
         if ($result->num_rows > 0) {
             $row = mysqli_fetch_assoc($result);
             $_SESSION['username'] = $row['username'];
-            header("Location: action/admin.php");
+            header("Location: admin.php");
         } else {
             echo "<script>alert('Email atau password Anda salah. Silahkan coba lagi!')</script>";
         }
     }
 ?>
-<html>
+<!DOCTYPE html>
     <head>
         <link rel="stylesheet" href="action/style.css" >
         <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="http://localhost/MbahDagang/index.php">Home</a></li>
-                <li><a href="#">Kontak</a></li>
-                <li><a href="#">Tentang</a></li>
-                <li><a href="#">Cari</a></li>
-            </ul> 
-        </nav>
-    </header>
     <body>
+        <!-- <div>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="http://localhost/MbahDagang/index.php">Home</a></li>
+                    <li><a href="#">Kontak</a></li>
+                    <li><a href="#">Tentang</a></li>
+                    <li><a href="#">Cari</a></li>
+                </ul> 
+            </nav>
+        </header>
+        </div> -->
             <div>
             <div class="row">
             <!-- left column -->
@@ -51,28 +53,31 @@
                     <h3 class="card-title">Login <small> Validasi Admin</small></h3>
                 </div>
                 <!-- /.card-header -->
+
                 <!-- form start -->
                 <form id="quickForm">
                     <div class="card-body">
                     <div class="form-group">
                         <label for="exampleInputEmail1">Username</label>
-                        <input type="email" name="username" class="form-control" id="exampleInputEmail1" value="<?php echo $username; ?>" placeholder="Masukan username">
+                        <input type="text" name="username" class="form-control" id="exampleInputEmail1" value="<?php echo $username; ?>" placeholder="Masukan username">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Password</label>
                         <input type="password" name="password" class="form-control" id="exampleInputPassword1"  value="<?php echo $_POST['password']; ?>" placeholder="Password">
                     </div>
-                    <!-- validation method -->
+
+                    <!-- validation method
                     <div class="form-group mb-0">
                         <div class="custom-control custom-checkbox">
                         <input type="checkbox" name="terms" class="custom-control-input" id="exampleCheck1">
                         <label class="custom-control-label" for="exampleCheck1">I agree to the <a href="#">terms of service</a>.</label>
                         </div>
                     </div>
-                    </div>
+                    </div> -->
+
                     <!-- /.card-body -->
                     <div class="card-footer">
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="submit" class="btn btn-primary">Submit</button>
                     </div>
                 </form>
                 </div>
@@ -97,39 +102,39 @@
             });
             $('#quickForm').validate({
                 rules: {
-                email: {
-                    required: true,
-                    email: true,
-                },
-                password: {
-                    required: true,
-                    minlength: 5
-                },
-                terms: {
-                    required: true
-                },
+                    username: {
+                        required: true,
+                        username: true,
+                    },
+                    password: {
+                        required: true,
+                        minlength: 5
+                    },
+                    terms: {
+                        required: true
+                    },
                 },
                 messages: {
-                email: {
-                    required: "Please enter a email address",
-                    email: "Please enter a valid email address"
-                },
-                password: {
-                    required: "Please provide a password",
-                    minlength: "Your password must be at least 5 characters long"
-                },
-                terms: "Please accept our terms"
+                    username: {
+                        required: "Please enter a email address",
+                        username: "Please enter a valid email address"
+                    },
+                    password: {
+                        required: "Please provide a password",
+                        minlength: "Your password must be at least 5 characters long"
+                    },
+                    terms: "Please accept our terms"
                 },
                 errorElement: 'span',
                 errorPlacement: function (error, element) {
-                error.addClass('invalid-feedback');
-                element.closest('.form-group').append(error);
+                    error.addClass('invalid-feedback');
+                    element.closest('.form-group').append(error);
                 },
                 highlight: function (element, errorClass, validClass) {
-                $(element).addClass('is-invalid');
+                    $(element).addClass('is-invalid');
                 },
                 unhighlight: function (element, errorClass, validClass) {
-                $(element).removeClass('is-invalid');
+                    $(element).removeClass('is-invalid');
                 }
             });
             });
