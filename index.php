56efa3ba8711085138702e28c49e175f84ea6b40
Made Agus Andi Gunawan <49284230+joeinus134131@users.noreply.github.com>
2022-11-10 09:19:58 +0700

    update login

--- index.php
+++ index.php
@@ -5,18 +5,17 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> 
 </head>
-<header>
-	<nav>
-        <ul>
-            <li><a href="http://localhost/MbahDagang/index.php">Home</a></li>
-            <li><a href="#">Kontak</a></li>
-            <li><a href="#">Tentang</a></li>
-            <li><a href="#">Cari</a></li>
-        </ul> 
-    </nav>
-</header>
 <body>
-	<div>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="http://localhost/MbahDagang/index.php">Home</a></li>
+				<li><a href="#">Kontak</a></li>
+				<li><a href="#">Tentang</a></li>
+				<li><a href="#">Cari</a></li>
+			</ul> 
+		</nav>
+	</header>
 		<h2 class="text-info">Pencarian berdasarkan Nama Barang</h2>
 		<form action="" method="POST">	
 			<input class="form-control" name="cari" id="cari" type="text" placeholder="cari nama barang .." > <br>
@@ -71,6 +70,5 @@
 		<div class='tengah'>
 			<a target="_blank" href="action/cetak.php">Cetak PDF</a>
 		</div>	
-	</div> 
 </body>
 </html>
